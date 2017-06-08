f7f8635e1e88c545186c78b9852581655c32e84e
jinglecjy <593775823@qq.com>
2017-06-08 23:59:52 +0800

    bucket sort

---
+++ sort_algorithm/bucket_sort.cpp
@@ -0,0 +1,135 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct node {
+    int value;
+    struct node *next;
+};
+
+void merge_sort(struct node *(*first));
+struct node *findmiddle(struct node *(*first));
+struct node *merge(struct node *(*first), struct node *(*second_first));
+void print_list(struct node *first);
+void free_list(struct node *first);
+
+int main(){
+	int situation;
+    while(scanf("%d", &situation) != EOF) {     
+        for(int j = 0; j < situation; j++) {
+            // Create 10000 buckets
+			struct node *harsing[10000] = {NULL};
+            for(int i=0; i<10000; i++){
+            	harsing[i] = (node*)malloc(sizeof(node));
+            	harsing[i]->next = NULL;
+			}
+			
+			// Throw source[i] to the corresponding bucket by its highest five digits
+			int amount;  // number of the nodes       
+            scanf("%d", &amount);			 
+            for(int i = 0; i < amount; i++) {
+                struct node *new_node = (node *)malloc(sizeof(node));
+                scanf("%d", &(new_node->value));
+                // Insert to the list from the front position
+                new_node->next = harsing[(new_node->value)/1000]->next; 
+                harsing[(new_node->value)/1000]->next = new_node;
+            }
+            
+            // Sort and output the linked buckets
+            for(int i=0; i<10000; i++){
+            	if(harsing[i] != NULL){
+            		merge_sort(&(harsing[i]->next));
+		            print_list(harsing[i]->next);
+		            free_list(harsing[i]);
+				}
+			}
+            
+        }
+    }
+	
+	return 0;
+} 
+
+void merge_sort(struct node *(*first)){
+	// When there is no data or just one data we don't need to separate it but just return it
+	if(*first != NULL && (*first)->next != NULL){
+		// Find the middle node and separate
+		struct node *middle = findmiddle(&*first);
+		// Merge sort the first part of the list
+		merge_sort(first);
+		// Merge sort the second part of the list
+		merge_sort(&middle);
+		// Merge these two separated lists
+		*first = merge(&*first, &middle);
+	}
+}
+
+struct node *findmiddle(struct node *(*first)){
+	// For safety(may not be used)
+	if(*first == NULL)
+		return NULL;
+		
+	struct node *middle = *first;       // The pointer move with one step
+	struct node *temp = (*first)->next; // The pointer move with two steps
+	while(temp != NULL){ // For safety move
+		temp = temp->next;
+		if(temp != NULL){ // For safety move
+			temp = temp->next;
+			middle = middle->next;
+		}
+	}
+	// Store the start pointer of the second part of the list
+	struct node *second = middle->next;
+	// Separate the list into two parts
+	middle->next = NULL;
+	// Return the pointer of the head of the second part of the list
+	return second;
+}
+
+struct node *merge(struct node *(*first), struct node *(*second_first)){
+	// The dump node, which it's convenient for us to link the list
+	struct node *final_first = (node *)malloc(sizeof(struct node));
+	struct node *final_last = &*final_first; // The pointer for the last node of the result list
+	
+	// Merge these two lists when these two lists are not empty
+	while(*first != NULL && *second_first != NULL){
+		// The head of first list not bigger than the head of second list
+		if((*first)->value <= (*second_first)->value){
+			final_last->next = *first; // Link the head of the first list to the result list
+			final_last = *first; // Renew the pointer for the last node of the result list
+			*first = (*first)->next; // Delete the head of the first list
+		}
+		// The head of first list bigger than the head of second list
+		else{
+			final_last->next = *second_first; // Link the head of the second list to the result list
+			final_last = *second_first; // Renew the pointer for the last node of the result list
+			*second_first = (*second_first)->next; // Delete the head of the second list
+		}
+	}
+	// Merge the extra part of the list
+	if(*first == NULL)
+		final_last->next = *second_first;
+	else
+		final_last->next = *first;
+		
+	// Return the real first node
+	return final_first->next;
+}
+
+void print_list(struct node *first) {
+	if(first==NULL) return;
+    for(struct node *temp = first->next; temp != NULL; temp = temp->next) {
+        printf("%d\n", first->value);
+        first = temp;
+    }
+    printf("%d\n", first->value);
+}
+
+void free_list(struct node *first) {
+	if(first==NULL) return;
+    for(struct node *temp = first->next; temp != NULL; temp = temp->next) {
+        free(first);
+        first = temp;
+    }
+    free(first);
+}    
